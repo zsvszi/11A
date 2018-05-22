b30590625422f4d1a523f456e6e2fb3801f25f95
hallgato9 <hallgato9@CADCAM1-09.ktiszk.local>
2018-05-22 10:23:59 +0200

    11A érettségi feladat

---
+++ Zsvszi/src/Iskola.java
@@ -0,0 +1,20 @@
+
+public class Iskola {
+
+	public static void main(String[] args) 
+	{
+		String line = "1;Kocsis Flóra;9A;5;4;4;5;2;4;1" ;
+		String[] data = line.split(";") 			;
+		int jegyek[] = new int[line.length()-3] 	;
+		double atlag = 0 							;
+		for (int i = 3  ; i < data.length;i++)
+		{
+			int temp = Integer.parseInt(data[i])	;
+			System.out.println(temp)				;
+			atlag = atlag + temp 					;
+		}
+		atlag = atlag / (data.length-3) 			;
+		System.out.printf("Tanulmányi átlag:%.2f",atlag);
+	}
+
+}
